c05b27e6bdd36215b950ad372f22f3732605d2b8
MrChatush <vip.bukbuk@mail.ru>
2026-04-26 02:23:38 +0300

    quitButton

---
+++ Assets/Scripts/QuitGameButton.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class QuitGameButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
